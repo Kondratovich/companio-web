213e2819bb0608016c66c900d309959a7344392f
Artsiom Kondratovich <artsiom.kondratovich@billwerk.com>
2023-04-25 11:47:16 +0300

    login/welcome page refactoring

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
+interface NasaPicture {
+    hdurl: string;
+    date: string;
+}
+
 @Component({
     selector: 'app-start-page',
     templateUrl: './start-page.component.html'
@@ -14,11 +19,18 @@ export class StartPageComponent implements OnInit {
         private http: HttpClient
     ) { }
 
+    backgroundPicture: string = '';
     isAuthenticated: boolean = false;
 
     ngOnInit(): void {
         this.isAuthenticated = this.authService.isAuthenticated();
-        this.http.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-          .subscribe(response => console.log(response));
-      }
+        this.http.get<NasaPicture>('https://api.nasa.gov/planetary/apod?api_key=hr8EGW7RfDZOXqc0TE63xcAwsvzHsoRIZWzPEq8Z')
+            .subscribe(response => this.backgroundPicture = response.hdurl);
+    }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['/login'])
+        this.isAuthenticated = false;
+    }
 }
